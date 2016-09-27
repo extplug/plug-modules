7eaee12831d356f9d23251729716ccf6ecaa9c46
René Kooi <renee@kooi.me>
2016-09-27 17:14:44 +0200

    match: fix already-found check

--- src/requirejs-finder/match.js
+++ src/requirejs-finder/match.js
@@ -1,15 +1,20 @@
 import internalGetMatcher from './internalGetMatcher';
+import rewrittenModuleName from './rewrittenModuleName';
 
 export default function match(fn) {
   const matcher = (context) => {
     const defines = context.target;
     for (const name in defines) if (defines.hasOwnProperty(name)) {
+      // Skip empty modules.
+      if (!defines[name]) {
+        continue;
+      }
       // Skip modules that were already found.
       if (rewrittenModuleName in defines[name]) {
         continue;
       }
 
-      if (defines[name] && fn(defines[name], name, context)) {
+      if (fn(defines[name], name, context)) {
         return name;
       }
     }
